de3d14e6f45b6843db897f934eea7cec8182c8b2
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-09-10 23:00:39 +0530

    Update Subarray with given sum.java

--- Subarray with given sum.java
+++ Subarray with given sum.java
@@ -6,9 +6,7 @@ import java.io.*;
 class Main{
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int t = sc.nextInt();
-
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
             int s = sc.nextInt();
